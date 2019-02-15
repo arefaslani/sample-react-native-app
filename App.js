56b0aef12ec28654828fac5f63107ef0eac97815
Aref Aslani <arefaslani@gmail.com>
2019-02-15 10:14:32 +0330

    add app directory

--- App.js
+++ App.js
@@ -1,21 +1,8 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import Home from "./app/views/Home"
 
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+    return <Home />;
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
