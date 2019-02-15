2ac56c0bbcad603822f959ad094ebcddafdc1c57
Aref Aslani <arefaslani@gmail.com>
2019-02-15 23:24:45 +0330

    add brand colors to navigation component

--- app/components/Navigation/index.js
+++ app/components/Navigation/index.js
@@ -6,15 +6,37 @@ import {
 
 import HomeScreen from "app/views/Home";
 
-const HomeStack = createStackNavigator({
-  Home: HomeScreen
-});
+const defaultStackNavigationOptions = {
+  headerStyle: {
+    backgroundColor: "#feeb3e"
+  }
+};
+
+const HomeStack = createStackNavigator(
+  {
+    Home: HomeScreen
+  },
+  {
+    defaultNavigationOptions: defaultStackNavigationOptions
+  }
+);
 
 export default createAppContainer(
-  createBottomTabNavigator({
-    Home: {
-      screen: HomeStack,
-      navigationOptions: HomeScreen.navigationOptions
+  createBottomTabNavigator(
+    {
+      Home: {
+        screen: HomeStack,
+        navigationOptions: HomeScreen.navigationOptions
+      }
+    },
+    {
+      tabBarOptions: {
+        style: {
+          backgroundColor: "#feeb3e"
+        },
+        inactiveTintColor: "#000",
+        activeTintColor: "#000"
+      }
     }
-  })
+  )
 );
